064c5f39db3971265304d7ad9e1c221c67e93fd0
x3kristx <kristo.lember2@gmail.com>
2025-10-23 16:30:32 +0300

    initialize book routes with CRUD endpoints

---
+++ src/routes/book.routes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const router = Router();
+
+
+
+router.get('/books', ()) => {});
+router.get('/books/:id', ()) => {});
+router.post('/books', ()) => {});
+router.put('/books', ()) => {});
+router.delete('/books', ()) => {});
+
+export default router;
